09746344b40732b8c64974fedda2cb1d7c8d4d41
Nikola Kelava <nikola.kelava94@gmail.com>
2022-03-08 20:08:32 +0100

    Add post partial

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get("/",function(req, res) {
 
 
 app.get("/about", function(req, res) {
-	res.render("about", { headerContent: aboutHeaderContent });
+	res.render("about", { headerContent: aboutHeaderContent, res: res });
 });
 
 
