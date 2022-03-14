78a696b2b4d519bece0ccf5608ccc8cf161c5fb9
Nikola Kelava <nikola.kelava94@gmail.com>
2022-03-14 22:51:54 +0100

    Change port

--- app.js
+++ app.js
@@ -110,6 +110,6 @@ app.post("/post", function(req, res) {
 });
 
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   	console.log("Server started on port 3000...");
 });
